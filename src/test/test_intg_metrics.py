862f6731197e1192d2c76095d063f2002fd07595
Michael Penhallegon <mike@hematite.tech>
2022-08-20 11:38:20 -0700

    added starlette_prometheus middleware and metrics route, added custom metric

--- src/test/test_intg_metrics.py
+++ src/test/test_intg_metrics.py
@@ -4,11 +4,39 @@ from unittest.mock import patch
 
 
 @pytest.mark.asyncio
-async def test_metrics_route(mock_db):
+async def test_metrics_route__status_content_type(mock_db):
     from ui_mongo import api
     async with TestClient(api.app) as test_client:
         with patch("ui_mongo.globals.db", new=mock_db):
             await test_client.get("/job_activity/")
             metrics_resp = await test_client.get("/metrics/")
             assert metrics_resp.status_code == 200
-            assert metrics_resp.headers.get("content-type") == "text"
+
+            # ,/metrics/ route should be plain text
+            assert "text/plain" in metrics_resp.headers.get("content-type")
+
+        assert any([
+            line
+            for line in metrics_resp.text.split("\n")
+            if "ui_mongo_entry_transactions" in line
+            ])
+
+@pytest.mark.asyncio
+async def test_metrics_route__test_app(metrics_test_app):
+    async with TestClient(metrics_test_app) as test_client:
+        await test_client.get("/foo/")
+        metrics_resp = await test_client.get("/metrics/")
+        assert metrics_resp.status_code == 200
+        assert "text/plain" in metrics_resp.headers.get("content-type")
+
+        # make sure ui_mongo metrics in response
+        entry_transaction_lines = [
+                                    line
+                                    for line in metrics_resp.text.split("\n")
+                                    if "ui_mongo_entry_transactions" in line
+                                    ]
+        assert any(entry_transaction_lines)
+        assert any([
+            line
+            for line in entry_transaction_lines
+            if "get_foo" in line])
--- src/ui_mongo/api.py
+++ src/ui_mongo/api.py
@@ -4,16 +4,20 @@ from fastapi import FastAPI
 from fastapi.middleware.gzip import GZipMiddleware
 
 from ui_mongo.routers import health, job_activity
+from starlette_prometheus import metrics, PrometheusMiddleware
 
 # constants & variables
 log = logging.getLogger(__name__)
 app = FastAPI()
+
 
 # Routers
 app.include_router(health.router)
 app.include_router(job_activity.router)
 
 app.add_middleware(GZipMiddleware)
+app.add_middleware(PrometheusMiddleware)
+app.add_route("/metrics", metrics)
 
 
 # Startup & Shutdown
